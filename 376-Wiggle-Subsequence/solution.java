2ec33e88bb4c697dfb116eb07f42595dbad46397
athrun9312 <athrun.9312@qq.com>
2016-08-30 12:21:47 -0400

    [376. Wiggle Subsequence][Accepted]committed by LeetCode Extension

--- 376-Wiggle-Subsequence/solution.java
+++ 376-Wiggle-Subsequence/solution.java
@@ -1,17 +1,22 @@
 public class Solution {
     public int wiggleMaxLength(int[] nums) {
+        if(nums.length <= 1)
+            return nums.length;
         int k = 1;
         while(k<nums.length && nums[k] == nums[k-1]) k++;
         if(k == nums.length)
             return 1;
-        int count = 2;
-        boolean increasing = nums[k]<nums[k-1];
-        for(int i=k; i<nums.length; i++) {
-            if((increasing && nums[i]>nums[i-1]) ||(!increasing && nums[i]<nums[i-1])) {
-                increasing = !increasing;
-                count++;
+        int maxLen = 2;
+        boolean isIncreasing = nums[k] > nums[k-1];
+        for(int i=k+1; i<nums.length; i++) {
+            if(isIncreasing && nums[i] < nums[i-1]) {
+                maxLen++;
+                isIncreasing = false;
+            } else if(!isIncreasing && nums[i] > nums[i-1]) {
+                maxLen++;
+                isIncreasing = true;
             }
         }
-        return count;
+        return maxLen;
     }
 }
